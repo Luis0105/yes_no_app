3cab45950f4c45979925f31edc3229f7810f83fd
Luis Santamaria <santamariaaguayoluisangel@gmail.com>
2025-03-25 23:45:54 -0600

    Chat Screen

---
+++ lib/presentation/chat/chat_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://static.myfigurecollection.net/upload/items/1/1931894-e7597.jpg'),
+          ),
+        ),
+        title: Text('Odalys'),
+        centerTitle: false,
+      ),
+    );
+  }
+}
