d2a6486121c10829ae404895d8cb961b5e931894
Luis Santamaria <santamariaaguayoluisangel@gmail.com>
2025-03-25 22:10:06 -0600

    Estilo Global

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/theme/app_theme.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,12 +11,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Yes No App',
       debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 2).theme(),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Material App Bar'),
         ),
-        body: const Center(
-          child: FilledButton(onPressed: onPressed, child: Text('Click me')),
+        body: Center(
+          child: FilledButton.tonal(
+              onPressed: () {}, child: const Text('Click me')),
         ),
       ),
     );
