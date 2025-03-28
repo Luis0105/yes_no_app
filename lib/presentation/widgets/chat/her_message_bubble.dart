ce6d84dbe94065ab6ad7b922dee1155ad6dbd1f2
Luis Santamaria <santamariaaguayoluisangel@gmail.com>
2025-03-28 14:30:14 -0600

    Mostrar mensaje

--- lib/presentation/widgets/chat/her_message_bubble.dart
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -22,12 +22,8 @@ class HerMessageBubble extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 5),
-
         _ImageBubble(),
-
         const SizedBox(height: 10),
-
-        // Todo: imagen
       ],
     );
   }
@@ -45,6 +41,16 @@ class _ImageBubble extends StatelessWidget {
           width: size.width * 0.7,
           height: 150,
           fit: BoxFit.cover,
+          loadingBuilder: (context, child, loadingProgress) {
+            if (loadingProgress == null) return child;
+
+            return Container(
+              width: size.width * 0.7,
+              height: 150,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: const Text("Toji Fushiguro está enviando una imagen"),
+            );
+          },
         ));
   }
 }
